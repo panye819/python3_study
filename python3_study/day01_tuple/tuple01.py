35ee01324c61871bc5514b910119b543d4797116
panye <panye819@gmail.com>
2017-07-03 07:43:36 +0800

    0703

---
+++ python3_study/day01_tuple/tuple01.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 2017年7月3日
+
+@author: FlyPiG8308
+'''
+print("您好！")
+
+number_list = [1,2,3,4]
+print(number_list)
+print("The number_list is : "+str(number_list))
