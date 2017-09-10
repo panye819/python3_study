0f1967e4858c7bb10ea5d4f2fafaca3a55b31670
castlepan <panye819@gmail.com>
2017-09-10 20:11:44 +0800

    add generator

--- 04-namespace/generator_01.py
+++ 04-namespace/generator_01.py
@@ -1,20 +1,27 @@
 #coding=utf-8
 #/usr/bin/env python3
 
-
-class Person(object):
-    __slots__=("name","age","addr")
-
+#生成器：
+#   在python中，这种一边循环一边计算的机制，称为生成器 generator
 
 
-P=Person()
+#a = [ x*2 for x in range(5)]
 
-P.name="laowang"
-P.age=33
-P.addr="Nanjing"
+g=(x*2 for x in range(1000))
 
+print("g is ",g)
 
-print(P.name+" "+str(P.age)+" "+P.addr)
+print("-"*30)
+#当一个函数中出现yield关键字的时候，这个函数就不再是普通的函数，而是生成器
 
-P.tel="123456"
-print(P.tel)
+def createNum():
+    print("---start---")
+    a,b=0,1
+    for i in range(5):
+        #程序执行到这里的时候，会暂停，然后返回yield后面的值
+        yield b
+        a,b=b,a+b
+    print("---stop---")
+
+a=createNum()
+next(a)
