55bfc1366aadfb4ab0138a12e02c3d2a93a4c9c8
panye819 <panye819@gmail.com>
2017-09-11 14:50:53 +0800

    add class decorator

--- 03-iterable/decorator_09.py
+++ 03-iterable/decorator_09.py
@@ -1,27 +1,23 @@
 #coding=utf-8
 #/usr/bin/env python3
 
-#带参数的装饰器
-def func_arg(arg=100):
-    def func(functionName):
-
-        def func_in():
-            print("----记录日志----arg=%s"%arg)
-            functionName()
-        return func_in
-    return func
-
-#装饰器带参数，在原有装饰器的基础上，设置外部变量
-
-#1、先执行func_arg("heihei")函数，这个函数return的结果是func这个函数的引用
-#2、@func
-#3、使用@func对test进行装饰
-
-
-#@func_arg("heihei")
-@func_arg()
+"""
+	类装饰器：
+		
+"""
+class Test(object):
+	"""docstring for Test"""
+	def __init__(self, func):
+		print("---初始化---")
+		print("func name is %s" %func.__name__)
+		self.__func = func
+	def __call__(self):
+		print("--装饰器中的功能---")
+		self.__func()
+@Test
 def test():
-    print("---test---")
+	print("---test---")
 
 
 test()
+
