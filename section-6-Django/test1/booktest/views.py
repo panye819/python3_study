ac633745997e8cb685f40abb9a95ef61327e287b
panye819 <panye819@gmail.com>
2017-09-18 13:48:02 +0800

    add project test2

--- section-6-Django/test1/booktest/views.py
+++ section-6-Django/test1/booktest/views.py
@@ -1,6 +1,20 @@
-#from django.shortcuts import render
-from django.http import *
+from django.shortcuts import render
+from .models import *
+# from django.http import *
+# from django.template import RequestContext,loader
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse('<h1><b>hello world</b></h1>')
+    # temp = loader.get_template('booktest/index.html')
+    # return HttpResponse(temp.render())
+    booklist = BookInfo.objects.all()
+    context={'list': booklist}
+    return render(request, 'booktest/index.html', context)
+
+
+def show(request, id):
+    book = BookInfo.objects.get(pk=id)
+    herolist = book.heroinfo_set.all()
+    context={'list': herolist}
+    return render(request, 'booktest/show.html', context)
