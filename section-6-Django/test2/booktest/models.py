af7ea2621e914f263611dc57cd41478a957ec70b
panye819 <panye819@gmail.com>
2017-09-19 17:35:06 +0800

    add mysite

--- section-6-Django/test2/booktest/models.py
+++ section-6-Django/test2/booktest/models.py
@@ -1,24 +1,41 @@
 from django.db import models
 
-# Create your models here.
-
 
 class BookInfoManager(models.Manager):
     def get_queryset(self):
-        return super(BookInfoManager, self).get_queryset().filter(isDelete=False)
+        return super(BookInfoManager,self).get_queryset().filter(isDelete=False)
+
+    def create(self,btitle,bpub_date):
+        b = BookInfo()
+        b.btitle = btitle
+        b.bpub_date = bpub_date
+        b.bread = 0
+        b.bcommet = 0
+        b.isDelete = False
+        return b
 
 
 class BookInfo(models.Model):
     btitle = models.CharField(max_length=20)
     bpub_date = models.DateTimeField(db_column='pub_date')
     bread = models.IntegerField(default=0)
-    bcomment = models.IntegerField(null=False)
+    bcommet = models.IntegerField(null=False)
     isDelete = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'bookinfo'
     books1 = models.Manager()
     books2 = BookInfoManager()
+
+    @classmethod
+    def create(cls, btitle, bpub_date):
+        b = BookInfo()
+        b.btitle = btitle
+        b.bpub_date = bpub_date
+        b.bread = 0
+        b.bcommet = 0
+        b.isDelete = False
+        return b
 
 
 class HeroInfo(models.Model):
