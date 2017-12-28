418bb84234e8aa3b7cbc77d19535af5c6d68a6c3
panye819 <panye819@gmail.com>
2017-12-28 11:11:03 +0800

    add note

--- WebScrap/demo.py
+++ WebScrap/demo.py
@@ -1,7 +1,37 @@
 from selenium import webdriver
 from pyquery import PyQuery as pq
+import pymysql
+import pymongo
+import redis
 
-driver = webdriver.PhantomJS()
-driver.get("http://www.baidu.com")
+# driver = webdriver.PhantomJS()
+# driver.get("http://www.baidu.com")
 # print(driver.page_source)
+
+
+doc = pq('<html>hello</html>')
+result = doc('html').text()
+print(result)
+print("-"*16)
+
+conn = pymysql.connect(host='localhost', user='root', password='',
+                       port=3306, db='mysql')
+cursor = conn.cursor()
+cursor.execute('select * from db')
+print(cursor.fetchone())
+
+print("-"*16)
+
+client = pymongo.MongoClient('localhost')
+db = client['newtestdb']
+db['table'].insert({'name': 'Bob'})
+print(db['table'].find_one({'name': 'Bob'}))
+
+print("-"*16)
+
+r = redis.Redis('localhost', 6379)
+r.set('name', 'Bob')
+print(r.get('name'))
+
+
 
